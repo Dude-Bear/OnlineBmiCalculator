51bfb7731c1be032c37974b4791d3d5d3cea2869
Bjoern <bjoern.schriewer@gmail.com>
2021-11-02 10:31:42 +0100

    Server v1 initial commit

---
+++ Server/src/main/java/de/hsemden/OnlineBmiCalculator/OnlineBmiCalculatorApplication.java
@@ -0,0 +1,13 @@
+package de.hsemden.OnlineBmiCalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBmiCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBmiCalculatorApplication.class, args);
+	}
+
+}
