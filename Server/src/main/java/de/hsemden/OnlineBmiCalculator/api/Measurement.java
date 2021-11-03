14a86f8dac4fa9437c58ce53659561a307f55ad2
Bjoern <bjoern.schriewer@gmail.com>
2021-11-03 13:45:47 +0100

    Server v2 add measurement and MeasurementRestController

---
+++ Server/src/main/java/de/hsemden/OnlineBmiCalculator/api/Measurement.java
@@ -0,0 +1,35 @@
+package de.hsemden.OnlineBmiCalculator.api;
+
+import java.time.LocalDate;
+
+/***
+ * The measurement api model class represents a measurement with its base attributes.
+ */
+public class Measurement {
+    /**
+     * The height in cm.
+     */
+    private int height;
+
+    /**
+     * The weight in kg.
+     */
+    private int weight;
+
+    /**
+     * The date of the measurement.
+     */
+    private LocalDate dateOfMeasurement;
+
+    /**
+     * The body mass index.
+     */
+    private double bmi;
+
+    /**
+     * The Nutritional status defined by the WHO.
+     * Source: https://www.euro.who.int/en/health-topics/disease-prevention/nutrition/a-healthy-lifestyle/body-mass-index-bmi
+     */
+    private int nutritionalStatus;
+
+}
---
+++ Server/src/main/java/de/hsemden/OnlineBmiCalculator/controller/MeasurementRestController.java
@@ -0,0 +1,33 @@
+package de.hsemden.OnlineBmiCalculator.controller;
+
+import de.hsemden.OnlineBmiCalculator.api.Measurement;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/***
+ * REST Controller for the measurement API.
+ *
+ * Offers the following REST Endpoints:
+ * GET /measurement -> returns a json that includes the bmi and the nutritional status.
+ */
+@RestController
+@RequestMapping(value = "measurement")
+public class MeasurementRestController {
+
+    /**
+     * Returns a measurement that includes the bmi.
+     *  TODO: Implementierung eines Servicesaufrufes.
+     *
+     * @param height Height in cm
+     * @param weight Weight in kg
+     * @return Calculation result
+     */
+    @GetMapping(produces = "calculation/json")
+    public ResponseEntity<Measurement> calculate(@RequestParam("height") int height,
+                                                 @RequestParam("weight") int weight) {
+        return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+    }
+
+
+}
